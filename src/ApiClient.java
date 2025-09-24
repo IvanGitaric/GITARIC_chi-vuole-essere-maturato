bc2dd7d8f1c9813bed7b1f2d1fc0740b740bd055
10934190 <10934190@itisrossi.vi.it>
2025-09-24 11:29:04 +0200

    Fase iniziale esercizio.

---
+++ src/ApiClient.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ApiClient {
+    private final HttpClient client = HttpClient.newHttpClient();
+
+    public String fetchQuestions(int amount, String type, String difficulty) {
+        //https://opentdb.com/api.php?amount=10
+        String url = "https://opentdb.com/api.php?amount=10";
+        HttpRequest request = HttpRequest.newBuilder()
+                .header("Content-Type", "application/json")
+                .uri(java.net.URI.create(url))
+                .GET()
+                .build();
+
+        HttpResponse<String> response;
+        try {
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        }catch (IOException | InterruptedException e){
+            return "Errore nella richiesta API";
+        }
+        return response.body();
+    }
+}
